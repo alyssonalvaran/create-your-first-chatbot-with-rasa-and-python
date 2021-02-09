1383e3d3b6ecaa94ac5e30b191236480ac51505a
alyssonalvaran <alvaran.alysson@gmail.com>
2021-02-10 03:21:29 +0800

    Add training data to find timezones

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,32 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+timezones = {
+    "London": "UTC+1:00",
+    "Manila": "UTC+8:00"
+}
+
+class ActionShowTimezone(Action):
+
+    def name(self) -> Text:
+        return "action_show_timezone"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        city = tracker.get_slot("city")
+        timezone = timezones.get(city)
+
+        output = "Sorry, I couldn't find the timezone of {}.".format(city)
+        if timezone:
+            output = "The timezone of {} is {}.".format(city, timezone)
+        
+        dispatcher.utter_message(text=output)
+
+        return []
